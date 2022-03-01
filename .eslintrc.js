1e72c3593ddc33d009e5bddf783655e4550d2682
Alija Sabic <sabic.alija@gmail.com>
2022-03-01 23:36:09 +0100

    Update eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,5 +25,10 @@ module.exports = {
     "generator-star-spacing": 0,
     // allow debugger during development
     "no-debugger": process.env.NODE_ENV === "production" ? 2 : 0,
+    semi: 0,
+    quotes: 0,
+    "space-before-function-paren": 0,
+    "comma-dangle": 0,
+    "no-unused-vars": 0,
   },
 };
